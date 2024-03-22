968a2b300fa69215fed67c5417d50f0755df2efe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-22 09:05:59 +0300

    Added some more migration logic

--- db/migrate.ts
+++ db/migrate.ts
@@ -1,5 +1,21 @@
+import { drizzle } from 'drizzle-orm/node-postgres'
+import { migrate } from 'drizzle-orm/node-postgres/migrator'
 import { Pool } from 'pg'
 
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL
+})
+
+const db = drizzle(pool)
+
+const main = async () => {
+	console.log('Migrations Started')
+	await migrate(db, { migrationsFolder: 'drizzle' })
+	console.log('Migrations Ended')
+	process.exit(0)
+}
+
+main().catch((error) => {
+	console.error(error)
+	process.exit(0)
 })
