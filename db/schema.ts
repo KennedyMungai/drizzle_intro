0004bb64fd233d9e435f9e771b76abf18222d91c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-03-22 08:56:36 +0300

    Exported the users pgTable

--- db/schema.ts
+++ db/schema.ts
@@ -1,6 +1,6 @@
 import { pgTable, text, uuid, varchar } from 'drizzle-orm/pg-core'
 
-const users = pgTable('users', {
+export const users = pgTable('users', {
 	id: uuid('id').defaultRandom().primaryKey(),
 	fullName: text('full_name').notNull(),
 	phone: varchar('phone_number', { length: 256 }).notNull()
